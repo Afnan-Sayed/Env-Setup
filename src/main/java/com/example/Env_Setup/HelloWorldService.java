066dcfbf852e48385ef0d033f97ac29f6c78b526
Afnan-Sayed <afnansayed50@gmail.com>
2025-08-19 18:02:09 +0300

    edit structure

--- src/main/java/com/example/Env_Setup/HelloWorldService.java
+++ src/main/java/com/example/Env_Setup/HelloWorldService.java
@@ -1,4 +1,4 @@
-package com.example.Env_Setup.Service;
+package com.example.Env_Setup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloWorldService
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(HelloWorldService.class, args);
     }
 }
