264813012204df85fdab1150eec3ad73bdeb1a63
Afnan-Sayed <afnansayed50@gmail.com>
2025-08-19 17:43:48 +0300

    Spring Hello World project

---
+++ src/test/java/com/example/Env_Setup/EnvSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Env_Setup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvSetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
